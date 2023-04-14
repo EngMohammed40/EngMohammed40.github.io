64488b108b9ec73a44d9b334ebcea91c7cc29c9e
EngMohammed40 <engmohammedahmed40@gmail.com>
2023-04-14 23:47:20 +0200

    First Commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.bxslider').bxSlider({
+    minSlides: 5,
+    maxSlides: 5,
+    moveSlides:1,
+    slideWidth: 360,
+    slideMargin: 10,
+    responsive:true,
+    auto:true,
+    controls:false
+  });   
+});
