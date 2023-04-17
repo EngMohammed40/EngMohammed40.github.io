7df6d106e2ab10b5b0545a34667ac6113f3d179d
EngMohammed40 <engmohammedahmed40@gmail.com>
2023-04-17 04:16:09 +0200

    Video

--- js/script.js
+++ js/script.js
@@ -10,3 +10,28 @@ $('.bxslider').bxSlider({
     controls:false
   });   
 });
+
+var player = videojs('video_js');
+
+$(window).scroll(function() {
+  var hT = $('.projects-counts').offset().top,
+      hH = $('.projects-counts').outerHeight(),
+      wH = $(window).height(),
+      wS = $(this).scrollTop();
+  if (wS > (hT+hH-wH)){
+    $('.count-number').each(function (index,value) {
+      let count  = $(this).data('count');
+      $(value).counto(count,2000)
+    });
+  }
+});
+
+$(window).scroll(function() {
+  var hT = $('.who-are-we').offset().top,
+      hH = $('.who-are-we').outerHeight(),
+      wH = $(window).height(),
+      wS = $(this).scrollTop();
+  if (wS > (hT+hH-wH)){
+    player.autoplay('any');
+  }
+});
